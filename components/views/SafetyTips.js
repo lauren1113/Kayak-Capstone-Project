09aa150182519cca36e72a140d91934034ec071b
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-08-22 04:40:44 -0500

    save under construction image

--- components/views/SafetyTips.js
+++ components/views/SafetyTips.js
@@ -2,4 +2,5 @@ export default () => `
   <h3 class="welcomeMessage" id="welcomeLine1">Safety Tips</h3>
   <br>
   <h3 class="CmgSoon">Coming Soon!</h3>
+  <img class="construction" src="https://github.com/lauren1113/Kayak-Capstone-Project/blob/master/Documents/Design/Kayak%20Photos/under-construction-unsplash.jpg?raw=true"/>
 `;
