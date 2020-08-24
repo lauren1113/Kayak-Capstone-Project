8ec7ca480a5dff33871f3db2466a6638df4186a7
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-08-24 16:52:15 -0500

    Save Header

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,12 @@ export default st => `
       <img id="headImg" src="https://github.com/lauren1113/Kayak-Capstone-Project/blob/master/Documents/Design/Kayak%20Photos/header-damir-spanic-1QL8fLEpxaE-unsplash.jpg?raw=true"/>
       <nav id="navButtons">${st.links}</nav>
     </div>
-    ${st.Logo}
-    <h3 class="welcomeLine1">${st.header}</h3>
-    <h3 class="welcomeLine2">${st.welcome}</h3>
+    <div>
+      ${st.Logo}
+    </div>
+    <div>
+      <h3 class="welcomeLine1">${st.header}</h3>
+      <h3 class="welcomeLine2">${st.welcome}</h3>
+    </div>
   </header>
 `;
