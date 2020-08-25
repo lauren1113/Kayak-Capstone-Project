72c39c5a15035845724679e839797e7948c856a9
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-08-25 00:28:43 -0500

    Save Nav Component

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,8 @@ export default links => `
   <nav id="navButtons">
     ${links
       .map(
-        link => `<button class="navBtn"><a href="#">${link.text}</a></button>`
+        link =>
+          `<button class="navBtn"><a href="/${link.title}" data-navigo>${link.text}</a></button>`
       )
       .join("")}
   </nav>
