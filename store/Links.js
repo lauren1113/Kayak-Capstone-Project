cd0d3b11b2f75fce98440f4dcf88e1ec13a921d0
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-08-25 22:15:13 -0500

    Change order of nav bar

--- store/Links.js
+++ store/Links.js
@@ -3,8 +3,8 @@ export default [
   { title: "KayakTracker", text: "Track My Kayak" },
   { title: "LakeFinder", text: "Find a Lake" },
   { title: "RiverFinder", text: "Find a River" },
-  { title: "KayakQuiz", text: "Kayak Quiz" },
   { title: "BeginnersGuide", text: "Beginner's Guide" },
+  { title: "KayakQuiz", text: "Kayak Quiz" },
   { title: "PackingList", text: "Packing List" },
   { title: "SafetyTips", text: "Safety Tips" }
 ];
--- store/PackingList.js
+++ store/PackingList.js
@@ -1,5 +1,5 @@
 export default {
   title: "Packing List",
-  Welcome: "Prepare for Your Next Adventure",
+  welcome: "Prepare for Your Next Adventure",
   page: "PackingList"
 };
