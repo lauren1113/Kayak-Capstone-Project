d7b96b59f0bbebe9953d7db1349c96b9277b29d3
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-08-26 21:04:38 -0500

    Save Changes -- calculating distance

--- index.js
+++ index.js
@@ -25,9 +25,11 @@ function render(st) {
 }
 function trackMyKayakFunctionality() {
   getMapData();
+  stopwatch();
 }
+
 function getMapData() {
-  const myMap = L.map("trackerMap"); // setView = starting lat & long + zoom level
+  const myMap = L.map("trackerMap");
   let firstTime = true;
   const successCallback = position => {
     if (position) {
@@ -54,7 +56,6 @@ function getMapData() {
     console.log(error);
   };
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  // track location as it changes -- add event listener to "start" on sw
   const watchId = navigator.geolocation.watchPosition(
     successCallback,
     errorCallback
@@ -79,15 +80,131 @@ function getMapData() {
   });
   tiles.addTo(myMap);
 }
-function somethingAboutAStopWatch() {
-  console.log("gonna be a bomb ass movie");
+
+// Stopwatch
+function stopwatch() {
+  let sw = {
+    etime: null,
+    edist: null,
+    epace: null,
+    ereset: null,
+    estart: null,
+    timer: null,
+    geoWatch: null,
+    now: 0,
+    init: function() {
+      // Get HTML Elements
+      sw.etime = document.getElementById("sw-time");
+      sw.edist = document.getElementById("sw-distance");
+      sw.epace = document.getElementById("sw-pace");
+      sw.ereset = document.getElementById("sw-resetButton");
+      sw.estart = document.getElementById("sw-startButton");
+
+      // Attach listeners
+      sw.ereset.addEventListener("click", sw.reset);
+      sw.ereset.disabled = false;
+      sw.estart.addEventListener("click", sw.start);
+      sw.estart.disabled = false;
+    },
+
+    /* [ACTIONS] */
+    tick: function() {
+      // tick(): update display if stopwatch running
+
+      // Calculate hours, mins, seconds
+      sw.now++;
+      let remain = sw.now;
+      let hours = Math.floor(remain / 3600);
+      remain -= hours * 3600;
+      let mins = Math.floor(remain / 60);
+      remain -= mins * 60;
+      let secs = remain;
+
+      // Update the display timer
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (mins < 10) {
+        mins = "0" + mins;
+      }
+      if (secs < 10) {
+        secs = "0" + secs;
+      }
+      sw.etime.innerHTML = hours + ":" + mins + ":" + secs;
+    },
+
+    start: function() {
+      // start(): start the stopwatch
+
+      sw.timer = setInterval(sw.tick, 1000);
+      sw.estart.value = "Stop";
+      sw.estart.removeEventListener("click", sw.start);
+      sw.estart.addEventListener("click", sw.stop);
+    },
+
+    stop: function() {
+      // stop(): stop the stopwatch
+
+      clearInterval(sw.timer);
+      sw.timer = null;
+      sw.estart.value = "Start";
+      sw.estart.removeEventListener("click", sw.stop);
+      sw.estart.addEventListener("click", sw.start);
+    },
+
+    reset: function() {
+      // reset(): reset the stopwatch
+
+      // Stop if running
+      if (sw.timer != null) {
+        sw.stop();
+      }
+
+      // Reset time
+      sw.now = -1;
+      sw.tick();
+    }
+  };
+
+  window.addEventListener("load", sw.init);
+}
+
+// Calculate Distance
+let startingLoc = (startingLoc.coords.latitude, startingLoc.coords.longitude);
+navigator.geolocation.getCurrentPosition(position => {
+  startingLoc = position;
+  startingLoc.coords.latitude;
+  startingLoc.coords.longitude;
+});
+console.log(startingLoc);
+
+let currentLoc = (currentLoc.coords.latitude, currentLoc.coords.longitude);
+navigator.geolocation.watchPosition(position => {
+  currentLoc = position;
+  currentLoc.coords.latitude;
+  currentLoc.coords.longitude;
+});
+console.log(currentLoc);
+
+navigator.geolocation.watchPosition(position => {
+  calculateDistance(
+    startingLoc.coords.latitude,
+    startingLoc.coords.longitude,
+    currentLoc.coords.latitude,
+    currentLoc.coords.longitude
+  );
+});
+
+function calculateDistance(lat1, lat2, lon1, lon2) {
+  const latDist = lat2 - lat1;
+  const lonDist = lon2 - lon1;
 }
 
 // FIREBASE USER DATABASE
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-const firebase = require("firebase/app");
+let firebase = require("firebase/app");
 
 // Add the Firebase products that you want to use
 require("firebase/auth");
