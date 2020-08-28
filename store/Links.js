0d356a830334f65f544548e5ad91773d2ca7aba6
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-08-28 00:27:05 -0500

    Save link order for texting

--- store/Links.js
+++ store/Links.js
@@ -1,8 +1,8 @@
 export default [
   { title: "Home", text: "Home" },
-  { title: "KayakTracker", text: "Track My Kayak" },
   { title: "LakeFinder", text: "Find a Lake" },
   { title: "RiverFinder", text: "Find a River" },
+  { title: "KayakTracker", text: "Track My Kayak" },
   { title: "BeginnersGuide", text: "Beginner's Guide" },
   { title: "KayakQuiz", text: "Kayak Quiz" },
   { title: "PackingList", text: "Packing List" },
