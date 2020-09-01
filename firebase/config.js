5877c65ff9cfb2421747fe925912807ea4d77378
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-09-01 14:38:38 -0500

    save Firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -1,7 +1,7 @@
 import "../env";
 
 export default {
-  apiKey: process.env.FIREBASE_API,
+  apiKey: AIzaSyCqruQYeJVKqGCZpcXX9rQyYkKpOgMh5Ug,
   authDomain: "kayak-fanatic-user-db.firebaseapp.com",
   databaseURL: "https://kayak-fanatic-user-db.firebaseio.com",
   projectId: "kayak-fanatic-user-db",
